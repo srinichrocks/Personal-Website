4481bb2f3b950a76db7cdb2fc513b011f016f7a0
srinichrocks <sraghavendran17@berkeley.edu>
2022-07-10 21:39:25 +0530

    another try

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,7 +2,6 @@ import 'react-multi-carousel/lib/styles.css';
 import 'react-modal-video/css/modal-video.min.css';
 import 'rc-drawer/assets/index.css';
 import 'typeface-dm-sans';
-import '../pages/styles.css'
 // import 'bootstrap/dist/css/bootstrap.min.css'
 
 export default function CustomApp({ Component, pageProps }) {
