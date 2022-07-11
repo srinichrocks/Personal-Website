1ea35b405410e8d57a998640e27438a85f11830c
srinichrocks <sraghavendran17@berkeley.edu>
2022-07-11 22:20:57 +0530

    tree structure change

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,12 @@ import theme from 'theme';
 import { NextSeo } from 'next-seo';
 import Head from 'next/head'
 import Layout from 'components/layout';
-import Home from './banner';
-import AboutMe from './about-me';
-import CourseWork from './projects';
-import Skills from './skills';
-import Journey from './journey';
-import ContactMe from './contact-me';
+import Home from '../sections/banner';
+import AboutMe from '../sections/about-me';
+import CourseWork from '../sections/projects';
+import Skills from '../sections/skills';
+import Journey from '../sections/journey';
+import ContactMe from '../sections/contact-me';
 
 export default function IndexPage() {
   return (
