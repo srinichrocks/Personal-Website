44daf66f965ae7540500cf59402a159af0a58fda
srinichrocks <sraghavendran17@berkeley.edu>
2022-07-17 03:49:16 +0530

    fix mobile width issue

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,6 +9,7 @@ export default function CustomApp({ Component, pageProps, router }) {
   return (
     <>
       <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
       <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"/>
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
